ee9b0552ec17901ad5099092ba0b5ff88584a2c8
DilawarDilsher <71361827+DilawarDilsher@users.noreply.github.com>
2021-01-05 19:57:35 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# idk37
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ sketch.js
@@ -0,0 +1,77 @@
+var paper ,box1 ,box2 ,box3 ,ground;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+paperImg=loadImage("paper.png");
+box1Img=loadImage("dustbingreen.png");
+
+}
+
+function setup() {
+	createCanvas(800, 700);
+    //paper= image ("paperImg");
+	box1=createSprite(500,610,20,100);
+	/*box2=createSprite(700,610,20,100);
+	box3=createSprite(600,660,200,10);
+	box1.shapeColor=color("blue");
+	box2.shapeColor=color("blue");
+	box3.shapeColor=color("blue");
+*/
+
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255);
+
+	paper = createSprite(100,648,25,25);
+	paper.x=World.mouseX;
+	paper.y=World.mouseY;
+	
+
+
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	//World.add(world, ground);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	paper.addImage("paper",paperImg);
+	box1.addImage("box1",box1Img);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(155);
+ 
+  paper.x=mouseX;
+	paper.y=mouseY;
+  
+    paper.scale = 0.5;
+    box1.scale = 0.5;
+
+
+
+	drawSprites();
+ 
+}
+
+function keyPressed() {
+
+if(keyCode === UP_ARROW){
+
+	Matter.Body.applyForce(paperOblect.body,paperOblect.body.position,{x:85,y:85});
+
+}
+
+
+
+}
+
